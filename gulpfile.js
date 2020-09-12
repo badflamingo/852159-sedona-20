2e4d94e6825e0b66e6594ca514b78cea7f02bbe1
Kate Sm <ktushsmit@gmail.com>
2020-09-12 12:13:35 +0200

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,32 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
 const sync = require("browser-sync").create();
+const del = require("del");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
 
+const clean = () => {
+  return del("build");
+};
 // Styles
 
 const styles = () => {
   return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(sync.stream());
+  .pipe(plumber())
+  .pipe(sourcemap.init())
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(csso())
+  .pipe(rename("styles.min.css"))
+  .pipe(sourcemap.write("."))
+  .pipe(gulp.dest("build/css"))
+  .pipe(sync.stream());
 }
 
 exports.styles = styles;
@@ -48,4 +59,50 @@ const watcher = () => {
 
 exports.default = gulp.series(
   styles, server, watcher
+);
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+};
+
+const webpx = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality:90}))
+  .pipe(gulp.dest("build/img"))
+}
+exports.webp = webpx;
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+  .pipe(svgstore())
+  pipe(rename("sprite.svg"))
+  pipe(gulp.dest("source/img"))
+}
+exports.sprite = sprite;
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ],
+  {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+}
+exports.copy = copy;
+
+const build = () => gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
 );
---
+++ source/js/menu.js
@@ -0,0 +1,23 @@
+
+function setup_navigation() {
+
+  var navMain = document.querySelector('.navigation');
+  var navButton = document.querySelector('.navigation-button');
+
+  navMain.classList.remove('navigation--nojs');
+  navMain.classList.remove('navigation--opened');
+  navMain.classList.add('navigation--closed');
+
+  navButton.onclick = function() {
+    if (navMain.classList.contains('navigation--closed')) {
+      navMain.classList.remove('navigation--closed');
+      navMain.classList.add('navigation--opened');
+      navButton.setAttribute('aria-label', 'Открыть меню');
+    } else {
+      navMain.classList.remove('navigation--opened');
+      navMain.classList.add('navigation--closed');
+      navButton.setAttribute('aria-label', 'Закрыть меню')
+    }
+  };
+}
+window.onload = setup_navigation
