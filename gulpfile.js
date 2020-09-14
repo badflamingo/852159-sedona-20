f309c7fc8086acd1ed9b195015065ccb924f1085
Kate Sm <ktushsmit@gmail.com>
2020-09-14 11:40:41 +0200

    9

--- gulpfile.js
+++ gulpfile.js
@@ -11,31 +11,34 @@ const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
+const uglify = require('gulp-uglify');
+const pipeline = require('readable-stream').pipeline;
+const htmlmin = require('gulp-htmlmin');
 
 const clean = () => {
   return del("build");
-};
-
-const html = () => {
-  return gulp.src("source/*.html",
-  {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
 };
 
 const copy = () => {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
-    "source/*.ico"
+    "source/*.ico",
+    "source/css/*.css"
   ],
   {
     base: "source"
   })
   .pipe(gulp.dest("build"));
-}
+};
+
+const minify_js = () => {
+  return pipeline(
+        gulp.src('source/js/*.js'),
+        uglify(),
+        gulp.dest('build/js')
+  );
+};
 
 // Styles0
 
@@ -52,7 +55,7 @@ const styles = () => {
   .pipe(sourcemap.write("."))
   .pipe(gulp.dest("build/css"))
   .pipe(sync.stream());
-}
+};
 
 
 // Server
@@ -67,7 +70,7 @@ const server = (done) => {
     ui: false,
   });
   done();
-}
+};
 
 
 // Watcher
@@ -75,7 +78,7 @@ const server = (done) => {
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
-}
+};
 
 
 const images = () => {
@@ -91,25 +94,33 @@ const webpx = () => {
   return gulp.src("source/img/**/*.{png,jpg}")
   .pipe(webp({quality:90}))
   .pipe(gulp.dest("build/img"))
-}
+};
 
 const sprite = () => {
   return gulp.src("source/img/**/icon-*.svg")
   .pipe(svgstore())
   pipe(rename("sprite.svg"))
   pipe(gulp.dest("source/img"))
-}
+};
 
-const build = gulp.series(clean, copy, styles, sprite, html);
+
+const minhtml = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+};
+
+const build = gulp.series(clean, copy, minify_js, styles, sprite, minhtml);
 const start = gulp.series(build, server, watcher);
 
+exports.minify_js = minify_js;
 exports.server = server;
 exports.webp = webpx;
 exports.sprite = sprite;
 exports.copy = copy;
 exports.styles = styles;
 exports.clean = clean;
-exports.hmtl = html;
+exports.minhtml = minhtml;
 
 exports.build = build;
 exports.start = start;
